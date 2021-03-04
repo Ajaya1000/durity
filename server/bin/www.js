6c1bf4765b74d7974256b8b8ad849ef6fdd4ad83
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-04 22:30:40 +0530

    deleted unused code

--- server/bin/www.js
+++ server/bin/www.js
@@ -3,9 +3,7 @@
 /**
  * Module dependencies.
  */
-
 var app = require('../app');
-var debug = require('debug')('server:server');
 var http = require('http');
 
 /**
@@ -58,9 +56,7 @@ function onError(error) {
     throw error;
   }
 
-  var bind = typeof port === 'string'
-    ? 'Pipe ' + port
-    : 'Port ' + port;
+  var bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
 
   // handle specific listen errors with friendly messages
   switch (error.code) {
@@ -83,8 +79,6 @@ function onError(error) {
 
 function onListening() {
   var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
+  var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
+  console.log('Listening on ' + bind);
 }
