64c36708e37c57958c7c780078561c8b66918f7a
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-05 08:49:25 +0530

    added Schema

--- server/bin/www.js
+++ server/bin/www.js
@@ -5,6 +5,7 @@
  */
 var app = require('../app');
 var http = require('http');
+var mongoose = require('mongoose');
 
 /**
  * Get port from environment and store in Express.
@@ -82,3 +83,14 @@ function onListening() {
   var bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
   console.log('Listening on ' + bind);
 }
+
+//Setting up default mongoose connection
+var mongoDB = 'mongodb://127.0.0.1/durity';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+//Getting the default connection
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('connected to MongoDB');
+});
