9993cd241e3604535bb6e503183382b4140aba62
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-05 11:36:15 +0530

    added crude for subject

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,28 +1,57 @@
 var express = require('express');
 var router = express.Router();
+var Subject = require('../schema/subject');
 
 router.use((req, res, next) => {
-  console.log('indexRouter called');
   next();
 });
 
 //CRUDEs
 
 //Read
-router.get('/', (req, res) => {
-  res.send('get called');
+router.get('/', async (req, res) => {
+  const subjects = await Subject.find({});
+  res.send(JSON.stringify(subjects));
 });
+
 //Create
-router.post('/', (req, res) => {
-  res.send('post called');
+router.post('/', async (req, res) => {
+  if (req.body.name) {
+    var newSubject = new Subject({ name: req.body.name });
+    newSubject = await newSubject.save();
+    res.send(JSON.stringify(newSubject));
+  } else {
+    res.status(500).send("Something isn't right ");
+  }
 });
-//Read
+
+//Update
 router.put('/', (req, res) => {
-  res.send('put called');
+  if (req.body.id) {
+    Subject.findByIdAndUpdate(req.body.id, req.body.update, (err, doc) => {
+      if (err) {
+        res.status(500).send("Something isn't right");
+      }
+
+      res.send(JSON.stringify(doc));
+    });
+  } else {
+    res.status(500).send("Something isn't right ");
+  }
 });
-//Read
+
+//Delete
 router.delete('/', (req, res) => {
-  res.send('delete called');
+  if (req.body.id) {
+    Subject.findByIdAndRemove(req.body.id, (err, doc) => {
+      if (err) {
+        res.status(500).send("Something isn't right");
+      }
+      res.send(JSON.stringify(doc));
+    });
+  } else {
+    res.status(500).send("Something isn't right ");
+  }
 });
 
 module.exports = router;
