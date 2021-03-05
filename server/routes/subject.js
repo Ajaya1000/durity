327f39232e9d96b88ae582586920a57559178ad8
Ajaya1000 <ajaymati1000@gmail.com>
2021-03-05 12:01:16 +0530

    added crud method for chapters

--- server/routes/subject.js
+++ server/routes/subject.js
@@ -1,26 +1,67 @@
 var express = require('express');
 var router = express.Router();
+var Subject = require('../schema/subject');
 
 router.use((req, res, next) => {
-  console.log('subjectRouter called');
   next();
 });
 
 //CRUDEs
 
 //Read
-router.get('/', (req, res) => {
-  res.send('get called');
+router.get('/:id', (req, res) => {
+  if (req.params.id) {
+    Subject.findById(req.params.id, (err, doc) => {
+      if (err) {
+        res.status(500).send("Something isn't right ");
+      } else {
+        res.send(JSON.stringify(doc));
+      }
+    });
+  } else {
+    res.status(500).send("Something isn't right ");
+  }
 });
+
 //Create
-router.post('/', (req, res) => {
-  res.send('post called');
+router.post('/:id', (req, res) => {
+  if (req.params.id) {
+    Subject.findById(req.params.id, async (err, parent) => {
+      if (err) {
+        res.status(500).send("Something isn't right ");
+      } else {
+        parent.chapters.push({
+          name: req.body.values.name,
+          description: req.body.values.description,
+        });
+        parent = await parent.save();
+        res.send(JSON.stringify(parent));
+      }
+    });
+  } else {
+    res.status(500).send("Something isn't right ");
+  }
 });
-//Read
-router.put('/', (req, res) => {
-  res.send('put called');
+
+//Update
+router.put('/:id', (req, res) => {
+  if (req.params.id) {
+    Subject.findById(req.params.id, async (err, parent) => {
+      if (err) {
+        res.status(500).send("Something isn't right ");
+      } else {
+        var chapter = parent.chapters.id(req.body.id);
+        chapter.name = req.body.values.name;
+        chapter.description = req.body.values.description;
+
+        parent = await parent.save();
+        res.send(JSON.stringify(parent));
+      }
+    });
+  }
 });
-//Read
+
+//Delete
 router.delete('/', (req, res) => {
   res.send('delete called');
 });
